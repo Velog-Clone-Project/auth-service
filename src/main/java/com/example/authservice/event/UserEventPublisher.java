dabdc93cbd2c2807c7fcd1fdf02ce767276f777e
lhdmir <lhdmir@naver.com>
2025-08-06 18:36:36 +0900

    refactor: event publisher exchange, routing key 환경설정에서 load

--- src/main/java/com/example/authservice/event/UserEventPublisher.java
+++ src/main/java/com/example/authservice/event/UserEventPublisher.java
@@ -1,5 +1,6 @@
 package com.example.authservice.event;
 
+import com.example.authservice.config.RabbitProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.stereotype.Service;
@@ -9,8 +10,12 @@ import org.springframework.stereotype.Service;
 public class UserEventPublisher {
 
     private final AmqpTemplate amqpTemplate;
+    private final RabbitProperties properties;
 
     public void sendUserCreatedEvent(UserCreatedEvent event) {
-        amqpTemplate.convertAndSend("user.exchange", "user.created", event);
+        amqpTemplate.convertAndSend(
+                properties.getExchanges().getUser(),
+                properties.getRoutingKeys().getUser().getCreated(),
+                event);
     }
 }
